6252ab833ebb1b2b39c6ed077b36d2b72b5fe94a
Jenni <jennifer.lindner@gravityandstorm.de>
2019-10-30 13:19:33 +0100

    Added new subfolders for subpages

---
+++ src/components/LandingPage/LandingPage.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './landingPage.scss';
+
+function LandingPage() {
+    return (
+        <div className="App">
+            <header className="App-header">
+                <p>
+                    Me tried my best.
+                </p>
+            </header>
+        </div>
+    );
+}
+
+export default LandingPage;
