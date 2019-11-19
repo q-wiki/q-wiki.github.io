2acac46550b0bcea982adc968650e837626dc616
Antonia Berger <AntoniaBerger@gmx.net>
2019-11-19 22:34:28 +0100

    added accordion, responsiveness still needs to be fixed

--- src/components/atoms/Tabs/Tab.js
+++ src/components/atoms/Tabs/Tab.js
@@ -3,12 +3,46 @@ import React, { Component } from 'react'
 import Tabs from 'react-responsive-tabs';
 
 import 'react-responsive-tabs/styles.css';
+import 'react-accessible-accordion/dist/fancy-example.css';
+
+import {
+    Accordion,
+    AccordionItem,
+    AccordionItemHeading,
+    AccordionItemButton,
+    AccordionItemPanel,
+} from 'react-accessible-accordion';
+
+
 import './tabs.scss';
+
+
+const accordionContent= [ {
+  title: 'Wrong correct answer',
+  category: 'Geography',
+  content: 'This is content 1',
+  userName: 'noobidinoob'
+},{
+  title: 'Wrong correct answer',
+  category: 'Space',
+  content: 'This is content 2',
+  userName: 'NoobMaster'
+},{
+  title: 'Duplicate Option',
+  category: 'History',
+  content: 'This is content 3',
+  userName: 'GodOfNoobs',
+}
+
+];
+
 
 const tabsContent = [
   {
     title: 'Open Reports',
-    content: 'This is a list of open reports!'
+    content:  <Accordion allowZeroExpanded>
+    {getAccordions()}
+    </Accordion>
   },
   {
     title: 'Closed Reports',
@@ -19,6 +53,24 @@ const tabsContent = [
     content: 'This is a list of Fixed Reports!'
   }
 ];
+
+function getAccordions() {
+  return accordionContent.map((accordionContent, index) => (
+    <AccordionItem key={index}>
+        <AccordionItemHeading>
+            <AccordionItemButton>
+                <p className="accordionButtonTitle">{accordionContent.title} [{accordionContent.category}] <span>reported by {accordionContent.userName}</span></p>
+            </AccordionItemButton>
+        </AccordionItemHeading>
+        <AccordionItemPanel>
+            <p>
+            {accordionContent.content}
+            </p>
+        </AccordionItemPanel>
+    </AccordionItem>
+
+  ));
+}
 
 function getTabs() {
   return tabsContent.map((tabsContent, index) => ({
@@ -33,7 +85,7 @@ function getTabs() {
 
 
 
-const Tab = () => <Tabs showMore={false} items={getTabs()} />;
+const Tab = () => <Tabs showMore={false} items={getTabs()} transformWidth={500}  />;
 
 
 export default Tab;
