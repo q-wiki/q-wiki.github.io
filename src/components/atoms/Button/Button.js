0d6dd81147c7b5985f25fe1447ac2fba18cb2751
heyarne <arne@schlueter.is>
2019-11-27 14:30:58 +0100

    Add sparql editor (#8)
    * Add YASQE sparql editor as React component
    
    * Set up WikidataQueryEditor
    
    * Implement query ui link

--- src/components/atoms/Button/Button.js
+++ src/components/atoms/Button/Button.js
@@ -4,13 +4,14 @@ import PropTypes from 'prop-types';
 import './button.scss';
 
 const Button = ({children, ...props }) => (
-   <button className="btn draw-border">
+   <button className="btn draw-border" onClick={props.onClick}>
        {children}
    </button>
 );
 
 Button.propTypes = {
-    children: PropTypes.string
+    children: PropTypes.string,
+    onClick: PropTypes.func
 };
 
 Button.defaultProps = {
