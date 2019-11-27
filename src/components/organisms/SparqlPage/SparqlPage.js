77e73770602c2b95990950e3890059b6a26cc4b5
heyarne <arne@schlueter.is>
2019-11-27 14:45:47 +0100

    Add result example to sparql page

--- src/components/organisms/SparqlPage/SparqlPage.js
+++ src/components/organisms/SparqlPage/SparqlPage.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState} from 'react'
 import Heading from '../../atoms/Heading/Heading'
 import WikidataQueryEditor from '../../molecules/WikidataQueryEditor/WikidataQueryEditor'
 
@@ -18,12 +18,35 @@ SERVICE wikibase:label { bd:serviceParam wikibase:language "[AUTO_LANGUAGE],en" 
 `
 
 export default function SparqlPage() {
+  const [response, setResponse] = useState(null)
+
   return <>
     <Heading type='H1'>The Stars are Sparqling tonight ✨</Heading>
     <WikidataQueryEditor
-      onQueryResult={(...args) => console.log('query result', args)}
+      onQueryResult={response => setResponse(response)}
       onQueryFailure={(...args) => console.log('query failure', args)}>
       {exampleQuery}
     </WikidataQueryEditor>
+
+    {/* NOTE: This is only here for demonstrative purposes */}
+    {response != null &&
+      <>
+        <Heading type='H2'>Query response</Heading>
+        <table>
+          <thead>
+            <tr>
+              {response.head.vars.map(label => <td>{label}</td>)}
+            </tr>
+          </thead>
+          <tbody>
+            {response.results.bindings.map(binding =>
+              <tr>
+                <td colspan='3'><pre>{JSON.stringify(binding)}</pre></td>
+              </tr>
+            )}
+          </tbody>
+        </table>
+      </>
+    }
   </>
 }
