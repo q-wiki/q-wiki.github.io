7920abee60bf9457d673ecfa1ae0eedff033e237
heyarne <arne@schlueter.is>
2019-12-04 13:45:40 +0100

    Add optional icon to button (#11)
    * Implement icon next to button label
    
    * Add option to make button small

--- src/components/atoms/Button/Button.js
+++ src/components/atoms/Button/Button.js
@@ -6,16 +6,21 @@ import Icon from '../Icon/Icon'
 import './button.scss';
 
 const Button = ({children, ...props }) => (
-   <button className="btn draw-border" onClick={props.onClick}>
+   <button className={`btn draw-border ${props.icon != null ? 'has-icon' : ''} ${props.small ? 'is-small' : ''}`} onClick={props.onClick}>
      {children}
      {/* You can configure an optional item; everything in props.icon will just be passed further down */}
-     {props.icon != null && <Icon {...props.icon} />}
+     {props.icon != null &&
+        <Icon width={props.icon.width || props.small ? 18 : 24}
+            height={props.icon.height || props.small ? 18 : 24}
+            {...props.icon} />}
    </button>
 );
 
 Button.propTypes = {
     children: PropTypes.string,
-    onClick: PropTypes.func
+    small: PropTypes.bool,
+    onClick: PropTypes.func,
+    icon: PropTypes.object
 };
 
 Button.defaultProps = {
