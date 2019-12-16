1528ddc17d3e031307924095619013b4fe624105
Antonia Berger <AntoniaBerger@gmx.net>
2019-12-16 15:04:48 +0100

    fixed footer, now always at bottom of the page

--- src/components/organisms/AboutPage/AboutPage.js
+++ src/components/organisms/AboutPage/AboutPage.js
@@ -1,8 +1,37 @@
 import React from 'react'
 import Heading from '../../atoms/Heading/Heading'
 
+import CardMember from '../../molecules/CardMember/CardMember'
+import TimeLineItem from '../../atoms/TimeLineItem/TimeLineItem'
+
+const images = [
+  '../../../../src/assets/images/member-1.png',
+  '../../../../src/assets/images/member-2.png',
+  '../../../../src/assets/images/member-3.png',
+  '../../../../src/assets/images/member-4.png',
+  '../../../../src/assets/images/member-5.png',
+  '../../../../src/assets/images/member-6.png',
+];
+
 export default function AboutPage() {
   return <>
-    <Heading type='H1'>About Us</Heading>
+
+        <CardMember link="https://github.com/AntoniaBe" backgroundImage={images[5]} headline={'Antonia Berger'} content={'Frontend Developer'}/>
+
+
+
+  <div>
+  <TimeLineItem
+    headline={'The Task'}
+    content={'The original group consisted of 6 master Students. As part of our masters program in International Media and Programming at the HTW Berlin, we were tasked to create a game in collaboration with Wikidata. During the summer semester of 2019 we worked several months to realize the project.'}
+    />
+  <TimeLineItem
+    alignmentRight
+    headline={'The End of the Beginning'}
+    content={'The result at the end of the semester was recieved positively, promising a good foundation onto which new teams could build upon. Not just amongst Wikidata but also from professors at the HTW, which lead to the continuation of the project during the next semester. While the original team mostly returned to the project, we also welcomed 2 new members to help us perfect the game'}
+    />
+  </div>
+  <br></br>
+  <br></br>
   </>
 }
--- src/components/organisms/LandingPage/LandingPage.js
+++ src/components/organisms/LandingPage/LandingPage.js
@@ -9,13 +9,11 @@ import Icon from '../../atoms/Icon/Icon'
 import Heading from '../../atoms/Heading/Heading'
 import Paragraph from '../../atoms/Paragraph/Paragraph'
 import MainLogo from '../../atoms/MainLogo/MainLogo'
-import TimeLineItem from '../../atoms/TimeLineItem/TimeLineItem'
 import Tab from '../../atoms/Tabs/Tab'
 import MenuItem from  '../../atoms/MenuItem/MenuItem'
 import Image from  '../../atoms/Image/Image'
 
 import Card from '../../molecules/Card/Card'
-import CardMember from '../../molecules/CardMember/CardMember'
 import Slideshow from '../../molecules/Slideshow/Slideshow'
 import NumberDisplay from '../../molecules/NumberDisplay/NumberDisplay'
 
@@ -24,14 +22,6 @@ import { cardsDataLandingPage, qWikiInfo, numberDisplay } from '../../../../src/
 import { Row, Col } from 'react-flexbox-grid';
 import Iframe from 'react-iframe'
 
-const images = [
-  '../../../../src/assets/images/member-1.png',
-  '../../../../src/assets/images/member-2.png',
-  '../../../../src/assets/images/member-3.png',
-  '../../../../src/assets/images/member-4.png',
-  '../../../../src/assets/images/member-5.png',
-  '../../../../src/assets/images/member-6.png',
-];
 
 const firstCardsDataLandingPageElement = cardsDataLandingPage.shift();
 
@@ -101,24 +91,6 @@ export default function LandingPage() {
       </div>
       </div>
     </Row>
-
-      <CardMember link="https://github.com/AntoniaBe" backgroundImage={images[5]} headline={'Antonia Berger'} content={'Frontend Developer'}/>
-
-
-
-<div>
-<TimeLineItem
-  headline={'The Task'}
-  content={'The original group consisted of 6 master Students. As part of our masters program in International Media and Programming at the HTW Berlin, we were tasked to create a game in collaboration with Wikidata. During the summer semester of 2019 we worked several months to realize the project.'}
-  />
-<TimeLineItem
-  alignmentRight
-  headline={'The End of the Beginning'}
-  content={'The result at the end of the semester was recieved positively, promising a good foundation onto which new teams could build upon. Not just amongst Wikidata but also from professors at the HTW, which lead to the continuation of the project during the next semester. While the original team mostly returned to the project, we also welcomed 2 new members to help us perfect the game'}
-  />
-</div>
-<br></br>
-<br></br>
 
     </>
   );
