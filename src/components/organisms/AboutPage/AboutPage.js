82959a044cea1dfc5eed74d0692bd06f8c8b2af3
Antonia Berger <AntoniaBerger@gmx.net>
2020-01-08 00:02:58 +0100

    changed member image, added screenshots, added timeline to about page

--- src/components/organisms/AboutPage/AboutPage.js
+++ src/components/organisms/AboutPage/AboutPage.js
@@ -4,10 +4,12 @@ import './aboutPage.scss';
 import Container75 from '../../atoms/Container75/Container75'
 
 import Heading from '../../atoms/Heading/Heading'
+import Image from  '../../atoms/Image/Image'
 import CardMember from '../../molecules/CardMember/CardMember'
 import TimeLineItem from '../../atoms/TimeLineItem/TimeLineItem'
 
-import { aboutUsData } from '../../../../src/constants/constants'
+import { aboutUsData, qWikiHistory } from '../../../../src/constants/constants'
+
 
 import { Row, Col } from 'react-flexbox-grid';
 
@@ -21,21 +23,42 @@ export default function AboutPage() {
         <Container75>
       <Heading  pallete="white" type="H1">The original Q-Wiki Project</Heading>
       </Container75>
+      <Row>
+      <div className="timeline-container">
 
+        {
+           qWikiHistory[0].firstPart.map((firstPart, index) =>
 
+           <div key={index} className="timeline-content-container right">
+             <div className="timeline-content">
+               <TimeLineItem
+                 headline={firstPart.heading}
+                 content={firstPart.text}
+                 />
+             </div>
+           </div>
+      )}
+      </div>
+        <Image  width={700} backgroundSrc={qWikiHistory[0].screenshots[0].src}/>
+    </Row>
+    <Row>
+          <Image  width={700} backgroundSrc={qWikiHistory[0].screenshots[1].src}/>
+          <div className="timeline-container">
 
+            {
+               qWikiHistory[0].secondPart.map((secondPart, index) =>
 
-  <div>
-  <TimeLineItem
-    headline={'The Task'}
-    content={'The original group consisted of 6 master Students. As part of our masters program in International Media and Programming at the HTW Berlin, we were tasked to create a game in collaboration with Wikidata. During the summer semester of 2019 we worked several months to realize the project.'}
-    />
-  <TimeLineItem
-    alignmentRight
-    headline={'The End of the Beginning'}
-    content={'The result at the end of the semester was recieved positively, promising a good foundation onto which new teams could build upon. Not just amongst Wikidata but also from professors at the HTW, which lead to the continuation of the project during the next semester. While the original team mostly returned to the project, we also welcomed 2 new members to help us perfect the game'}
-    />
-  </div>
+               <div key={index} className="timeline-content-container right">
+                 <div className="timeline-content">
+                   <TimeLineItem
+                     headline={secondPart.heading}
+                     content={secondPart.text}
+                     />
+                 </div>
+               </div>
+          )}
+          </div>
+        </Row>
   </div>
   <div className="members-container">
       <Heading type="H1">The Q-Wiki Team</Heading>
