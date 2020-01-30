f55ee3e53a1c104cd14d1ab04e1986cbcbccbca6
heyarne <arne@schlueter.is>
2020-01-30 16:08:18 +0100

    Content is king

--- src/components/organisms/ContactPage/ContactPage.js
+++ src/components/organisms/ContactPage/ContactPage.js
@@ -5,10 +5,32 @@ import Heading from '../../atoms/Heading/Heading'
 import Paragraph from '../../atoms/Paragraph/Paragraph'
 
 const ContactPage = () => <Container75>
-  <Heading>Contact</Heading>
-  <Paragraph>
-
-  </Paragraph>
+  <Heading type='H1'>Contact</Heading>
+  <div>
+    <Paragraph>
+      Malte Götz
+    </Paragraph>
+  </div>
+  <div>
+    <Paragraph>
+      Tannenhofweg 101
+    </Paragraph>
+  </div>
+  <div>
+    <Paragraph>
+      40627 Düsseldorf, Germany
+    </Paragraph>
+  </div>
+  <div>
+    <Paragraph>
+      Phone: +49 176 - 71712069
+    </Paragraph>
+  </div>
+  <div>
+    <Paragraph>
+      playstore@maltegoetz.com
+    </Paragraph>
+  </div>
 </Container75>
 
 ContactPage.displayName = 'ContactPage'
